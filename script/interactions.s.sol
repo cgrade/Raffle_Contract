fbff50372a740b6a607e70c6f3ec04d50d134c60
Abraham Elijah <gradeisageek@gmail.com>
2024-11-10 09:14:05 +0100

    interaction scripts refactored

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -9,7 +9,16 @@ import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VR
 import {LinkToken} from "../test/mocks/LinkToken.sol";
 import {CodeConstants} from "./HelperConfig.s.sol";
 
+/**
+ * @title CreateSubscription
+ * @dev This contract handles the creation of a VRF subscription.
+ */
 contract CreateSubscription is Script {
+    /**
+     * @notice Creates a subscription using configuration from HelperConfig.
+     * @return subId The ID of the created subscription.
+     * @return vrfCoordinatorV2_5 The address of the VRF Coordinator.
+     */
     function createSubscriptionUsingConfig() public returns (uint256, address) {
         HelperConfig helperConfig = new HelperConfig();
         address vrfCoordinatorV2_5 = helperConfig.getConfigByChainId(block.chainid).vrfCoordinatorV2_5;
@@ -17,6 +26,13 @@ contract CreateSubscription is Script {
         return createSubscription(vrfCoordinatorV2_5, account);
     }
 
+    /**
+     * @notice Creates a VRF subscription.
+     * @param vrfCoordinatorV2_5 The address of the VRF Coordinator.
+     * @param account The account to start broadcasting from.
+     * @return subId The ID of the created subscription.
+     * @return vrfCoordinatorV2_5 The address of the VRF Coordinator.
+     */
     function createSubscription(address vrfCoordinatorV2_5, address account) public returns (uint256, address) {
         console.log("Creating subscription on chainId: ", block.chainid);
         vm.startBroadcast(account);
@@ -27,12 +43,28 @@ contract CreateSubscription is Script {
         return (subId, vrfCoordinatorV2_5);
     }
 
+    /**
+     * @notice Runs the subscription creation process.
+     * @return subId The ID of the created subscription.
+     * @return vrfCoordinatorV2_5 The address of the VRF Coordinator.
+     */
     function run() external returns (uint256, address) {
         return createSubscriptionUsingConfig();
     }
 }
 
+/**
+ * @title AddConsumer
+ * @dev This contract allows adding a consumer to a VRF subscription.
+ */
 contract AddConsumer is Script {
+    /**
+     * @notice Adds a consumer contract to a VRF subscription.
+     * @param contractToAddToVrf The address of the consumer contract.
+     * @param vrfCoordinator The address of the VRF Coordinator.
+     * @param subId The ID of the subscription.
+     * @param account The account to start broadcasting from.
+     */
     function addConsumer(address contractToAddToVrf, address vrfCoordinator, uint256 subId, address account) public {
         console.log("Adding consumer contract: ", contractToAddToVrf);
         console.log("Using vrfCoordinator: ", vrfCoordinator);
@@ -42,6 +74,10 @@ contract AddConsumer is Script {
         vm.stopBroadcast();
     }
 
+    /**
+     * @notice Adds a consumer using configuration from HelperConfig.
+     * @param mostRecentlyDeployed The address of the most recently deployed contract.
+     */
     function addConsumerUsingConfig(address mostRecentlyDeployed) public {
         HelperConfig helperConfig = new HelperConfig();
         uint256 subId = helperConfig.getConfig().subscriptionId;
@@ -51,15 +87,25 @@ contract AddConsumer is Script {
         addConsumer(mostRecentlyDeployed, vrfCoordinatorV2_5, subId, account);
     }
 
+    /**
+     * @notice Runs the process of adding a consumer to a subscription.
+     */
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("Raffle", block.chainid);
         addConsumerUsingConfig(mostRecentlyDeployed);
     }
 }
 
+/**
+ * @title FundSubscription
+ * @dev This contract handles funding a VRF subscription.
+ */
 contract FundSubscription is CodeConstants, Script {
     uint96 public constant FUND_AMOUNT = 3 ether;
 
+    /**
+     * @notice Funds a subscription using configuration from HelperConfig.
+     */
     function fundSubscriptionUsingConfig() public {
         HelperConfig helperConfig = new HelperConfig();
         uint256 subId = helperConfig.getConfig().subscriptionId;
@@ -78,6 +124,13 @@ contract FundSubscription is CodeConstants, Script {
         fundSubscription(vrfCoordinatorV2_5, subId, link, account);
     }
 
+    /**
+     * @notice Funds a VRF subscription.
+     * @param vrfCoordinatorV2_5 The address of the VRF Coordinator.
+     * @param subId The ID of the subscription.
+     * @param link The address of the LINK token.
+     * @param account The account to start broadcasting from.
+     */
     function fundSubscription(address vrfCoordinatorV2_5, uint256 subId, address link, address account) public {
         console.log("Funding subscription: ", subId);
         console.log("Using vrfCoordinator: ", vrfCoordinatorV2_5);
@@ -97,6 +150,9 @@ contract FundSubscription is CodeConstants, Script {
         }
     }
 
+    /**
+     * @notice Runs the funding process for a subscription.
+     */
     function run() external {
         fundSubscriptionUsingConfig();
     }
